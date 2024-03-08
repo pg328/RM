72c45807a31c80006be133df6a1b22740e0e467b
Phil George <23001002+pg328@users.noreply.github.com>
2024-03-08 18:36:24 +0000

    fix: sort default batch number to be recent

--- src/server/api/routers/batch.ts
+++ src/server/api/routers/batch.ts
@@ -1,6 +1,8 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const batchRouter = createTRPCRouter({
-  getBatches: publicProcedure.query(({ ctx }) => ctx.db.batch.findMany()),
+  getBatches: publicProcedure.query(({ ctx }) =>
+    ctx.db.batch.findMany({ orderBy: { id: "desc" } }),
+  ),
   create: publicProcedure.mutation(async ({ ctx }) => ctx.db.batch.create({})),
 });
